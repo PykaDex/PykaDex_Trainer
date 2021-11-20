b8c6a67d6e591308fa15f91612f3891dda342ac0
Shellywell123 <benjamin@shellswell.com>
2021-11-20 11:59:46 +0000

    added WSL workaround script

---
+++ open_in_WSL_to_run.bat
@@ -0,0 +1,7 @@
+Rem This File is a workaround for running python scripts on GPU
+Rem Using native windows Cmd from WSL
+
+cd C:\\Users\benja\Programming\Python_Projects\PykaDex-Organisation\PykaDex_Trainer\
+python3 pytorch_training.py
+
+TIMEOUT 10
